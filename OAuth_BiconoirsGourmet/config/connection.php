d715ffdf10edc08e12830f8644ca8ebce11bf2c3
Dylainous <dylanjp10@outlook.com>
2026-05-28 17:16:14 -0500

    Update connection.php

--- OAuth_BiconoirsGourmet/config/connection.php
+++ OAuth_BiconoirsGourmet/config/connection.php
@@ -6,7 +6,7 @@ $capsule = new Capsule;
 
 $capsule->addConnection([
     'driver'   => 'pgsql',
-    'url'      => 'postgresql://postgres.syiqftmpmmpvjbpmqspp:Alexander12.Dalmatax1011@aws-1-us-east-2.pooler.supabase.com:6543/postgres',
+    'url'      => 'postgresql://postgres.teqgmzwpcwcelrpecdyq:VNcazVEpbUvpGyVJ@aws-1-us-east-1.pooler.supabase.com:5432/postgres',
     'charset'  => 'utf8',
     'prefix'   => '',
     'sslmode'  => 'require',
